18d1bdebacdfbcbcd6fbdf0336e2c483a0c28adb
Kennedy S. Del Rosario <kennydelrosario1996@gmail.com>
2020-12-23 22:42:33 +0800

    Added API and bin

--- API/server.js
+++ API/server.js
@@ -1,32 +1,19 @@
 //External Deps
-const hapi=require( "@hapi/hapi" )
-const mongodb=require( "mongodb" )
-const chalk=require( "chalk" )
+const Hapi = require('@hapi/hapi');
+const mongo = require( 'mongodb' );
+const chalk = require( 'chalk' );
 //External Deps
 
-const date = new Date()
-const dbName = "pse"
+//Globals
+const d = new Date()
 const db_url = "mongodb://localhost:27017"
-const client = mongodb.MongoClient( `${db_url}/${dbName}` , {
-	useNewUrlParser : true ,
-	useUnifiedTopology : true
+const dbName = "pse"
+const client = mongo.MongoClient( `${db_url}/${dbName}` , {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true,
 })
 const connection = client.connect()
-
-const resolve_address = ( type ) => {
-	let array = [];
-
-	Object.keys( ifaces ).forEach( ( ifname ) => {
-		ifaces[ ifname ].forEach( ( iface ) => {
-			//skip addresses that do not match with specified type and skip 127.0.0.1
-			if ( (iface.family).toUpperCase() !== type.toUpperCase() || iface.internal !== false ) { return 1 }
-
-			array.push( iface.address );
-		});
-	});
-
-	return array;
-}
+//Globals
 
 const log_msg = ( message , type , request ) => {
 
@@ -49,9 +36,9 @@ const log_msg = ( message , type , request ) => {
 
 	if ( request != null || request != undefined ) {
 		const ip = RequestIp.getClientIp(request)
-		console.log( `${date.toString().split( '(' )[0]}: ${chalk.keyword( color )(type)} : ${ip} : ${message}` )
+		console.log( `${d.toString().split( '(' )[0]}: ${chalk.keyword( color )(type)} : ${ip} : ${message}` )
 	} else {
-		console.log( `${date.toString().split( '(' )[0]}: ${chalk.keyword( color )(type)} : ${message}` )
+		console.log( `${d.toString().split( '(' )[0]}: ${chalk.keyword( color )(type)} : ${message}` )
 	}
 
 }
@@ -69,17 +56,16 @@ const check_db = async () => {
 		if ( cols.length == 0 || cols.length != require_cols.length ) {
 			for (var i = 0; i < require_cols.length; i++) {
 
-				if ( cols[i] != undefined ) {
-					if ( cols[i].name == require_cols[i] ) {
+				if (cols[i] !== undefined ) {
+					if( cols[i].name == require_cols[i] ) {
 						continue
 					}
 				}
 
 				await db.createCollection( require_cols[i] )
-
 			}
 
-			const insert = { APP_INITIALIZED : true }
+			const insert = { APP_INITIALIZED : false }
 			const config_col = db.collection( "config" )
 
 			await config_col.find( {} )
@@ -93,7 +79,7 @@ const check_db = async () => {
 						config_bulkOp.execute().then( ( err , res ) => {
 							( err.result.ok )
 								? log_msg( `check_db : Init Config Done` , 'INFO  ' )
-								: log_msg( `check_db : Init Config Failed` , 'INFO  ' )
+								: Log.Info( `check_db : Init Config Failed` , 'INFO  ' )
 						})
 					}
 				})
@@ -106,85 +92,55 @@ const check_db = async () => {
 
 const start = async() => {
 
-	const server = hapi.server({
-		port: 8000 ,
-		host: 'localhost' 
+	const server = Hapi.server({
+		port : 8000 , 
+		host : 'localhost'
 	})
 	const connect = connection
 
 	server.route({
-		method : 'GET' ,
-		path : '/' ,
-		handler : async ( request , h ) => {
-			return Promise.all( [ 'this is a microservice' ] )
-		}
-	})
 
-	server.route({
 		method : 'GET' ,
 		path : '/quote/{symbol}' ,
 		handler : async ( request , h ) => {
 			let params = request.params
 			let res = null
 			const retn = await connect.then( async () => {
-				const db = client.db( dbName )
-				const col = db.collection( "price" )
+				const db = client.db( dbName );
+				const col = db.collection( 'price' );
+				const find = { 'Date' : `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}` }
 
-				const find = { 'Date' : `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}` }
+				res = await col.find( find )
+							   .toArray()
+							   .then( data => {
 
-				const res = await col.find( find )
-									 .toArray()
-									 .then( data => {
-									 	for (var i = 0; i < data[0].stock_data.length; i++) {
-									 		if ( data[0].stock_data[i].symbol == params.symbol ) {
-									 			return Promise.all( [ data[0].stock_data[i] ] )
-									 		}
-									 	}
-									 	return Promise.all( [ "WRONG" ] )
-									 })
+									for (var i = 0; i < data[0].stock_data.length; i++) {
 
-				return Promise.all( [ res[0] ] )
+										if ( data[0].stock_data[i].symbol == params.symbol ) {
+											log_msg( `Quote ${params.symbol}` , 'INFO  ' )
+											return Promise.all( [ data[0].stock_data[i] ] )
+										}
 
+									}
+
+									log_msg( `Stock Code Invaliid` , 'INFO  ' )
+									return Promise.all( [ "WRONG" ] )
+							   })
+
+				return Promise.all( res )
 			})
 
-			log_msg( `Quote ${params.symbol}` , 'INFO  ' )
-
-			return ( retn[0] == "WRONG" )
-				? h.response( { status : retn[0] } ).code( 401 )
-				: h.response( { status : retn[0] } )
-
-			}
-
-	})
-
-	server.route({
-		method : 'GET' ,
-		path : '/{route}' ,
-		handler : ( request , h ) => {
-			let params = request.params
-
-			if ( params.route == "favicon.ico" ) {
-				return 0
-			} else {
-				log_msg( `from ${params.route} to /` , 'REDIR ' , request )
-				return h.redirect( '/' )
-			}
+			return h.response( { status : retn[0] } )
 
 		}
+
 	})
 
 	await server.start()
-	log_msg( `Started at ${server.info.uri}` , 'START ' )
+	log_msg( `Server start at ${server.info.uri}` , 'INFO  ' )
 
 }
-
-process.on( 'unhandledRejection' , ( err ) => {
-	console.log( err )
-	process.exit( 1 )
-})
 
 check_db()
 
 start()
-
-
